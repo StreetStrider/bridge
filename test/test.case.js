93a468128db2b9744b33b6513f9fb4bbe5377a40
Strider <strange.transistor@gmail.com>
2017-07-25 10:45:02 +0300

    workon fixtures.

--- test/test.case.js
+++ test/test.case.js
@@ -3,14 +3,22 @@
 declare var describe: Function;
 declare var it: Function;
 
+import rootpath from 'rootpath'
+
 import bridge from '../'
+
+var fromroot = rootpath(__dirname, '../', '../..')
+var fromfix  = fromroot.partial('fixt/')
 
 describe('bridge', () =>
 {
 	it('dev', () =>
 	{
+		process.chdir(fromfix('dev'))
+
 		var cfg = bridge()
 
-		console.log(cfg)
+		console.log(cfg._.main)
+		console.log(cfg.get('main'))
 	})
 })
