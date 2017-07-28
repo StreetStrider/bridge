1e7a2b37f7812140dc362b4dae341ddd5f233792
Strider <strange.transistor@gmail.com>
2017-07-28 17:14:52 +0300

    test: get, nsget.

---
+++ test/get.case.js
@@ -0,0 +1,39 @@
+/* @flow */
+
+import rootpath from 'rootpath'
+import { expect } from 'chai'
+
+import bridge from '../'
+
+var fromroot = rootpath(__dirname, '../', '../..')
+
+describe('bridge#get,nsget', () =>
+{
+	process.chdir(fromroot('fixt/get'))
+
+	var cfg = bridge()
+
+	it('get', () =>
+	{
+		expect(cfg.get('name')).eq('get')
+		expect(cfg.get('version')).eq('0.0.0')
+		expect(cfg.get('override')).eq('dev')
+		expect(cfg.get('main')).eq(true)
+		expect(cfg.get('dev')).eq(true)
+
+		expect(cfg.get('dev2')).eq(null)
+		expect(cfg.get('dev2', false)).eq(false)
+	})
+
+	it('nsget', () =>
+	{
+		expect(cfg.nsget('package', 'name')).eq('get')
+		expect(cfg.nsget('main', 'name')).eq(null)
+		expect(cfg.nsget('main', 'name', ':name')).eq(':name')
+
+		expect(cfg.nsget('package', 'override')).eq('package')
+		expect(cfg.nsget('main', 'override')).eq('main')
+		expect(cfg.nsget('merged', 'override')).eq('dev')
+		expect(cfg.nsget('all', 'override')).eq('dev')
+	})
+})
